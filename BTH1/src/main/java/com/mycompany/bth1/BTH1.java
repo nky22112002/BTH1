55741e053b61926a01a355ff6e4fd3278502ce41
nky22112002 <nky22112002@gmail.com>
2023-02-16 14:59:06 +0700

    cc

--- BTH1/src/main/java/com/mycompany/bth1/BTH1.java
+++ BTH1/src/main/java/com/mycompany/bth1/BTH1.java
@@ -13,7 +13,8 @@ public class BTH1 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        int i = 2;
-        i++;
+        int i = 0;
+        if(i > 1)
+            i++;
     }
 }
