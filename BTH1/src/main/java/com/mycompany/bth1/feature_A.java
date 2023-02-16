89bb938ad77d5e5e3bf2701f5e77e1d491cd1242
nky22112002 <nky22112002@gmail.com>
2023-02-16 14:06:59 +0700

    cap nhat form A

---
+++ BTH1/src/main/java/com/mycompany/bth1/feature_A.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.bth1;
+
+/**
+ *
+ * @author admin
+ */
+public class feature_A {
+    
+}
